f00629f21609d3203f7535de180d4a6a59ee9158
Brandon Samuel <brandon.samuel2010@gmail.com>
2023-05-02 15:50:48 -0400

    working on snow integration

--- aws/k3s-terraform-cluster/network_services/outputs.tf
+++ aws/k3s-terraform-cluster/network_services/outputs.tf
@@ -25,3 +25,7 @@ output "vpc_cidr_block" {
 output "public_subnets_cidr_blocks" {
   value = module.vpc.public_subnets_cidr_blocks
 }
+
+output "private_subnets_cidr_blocks" {
+  value = module.vpc.private_subnets_cidr_blocks
+}
