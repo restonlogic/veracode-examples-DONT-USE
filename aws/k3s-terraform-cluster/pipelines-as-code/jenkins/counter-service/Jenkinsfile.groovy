1d0f49619e00c928b93ff2611fb55e8e5e5cb5ed
Brandon Samuel <brandon.samuel2010@gmail.com>
2023-05-09 12:27:47 -0400

    working on snow integration

--- aws/k3s-terraform-cluster/pipelines-as-code/jenkins/counter-service/Jenkinsfile.groovy
+++ aws/k3s-terraform-cluster/pipelines-as-code/jenkins/counter-service/Jenkinsfile.groovy
@@ -67,7 +67,7 @@ pipeline {
         steps {
           script {
             dir("${repoFolder}") {
-            change_sys_id = snow.changeRequest("Deploying $image to $env kubernetes cluster in region $region", "$image is currently being built and deployed by jenkins to $env kubernetes cluster in region $region, Link to build: ${buildUrl}", "Commit Hash: ${build_tag}, Application: ${image}, Environment: ${env}, Region: ${region}", null, null, null, null, null, null)
+            snow.changeRequest("Deploying $image to $env kubernetes cluster in region $region", "$image is currently being built and deployed by jenkins to $env kubernetes cluster in region $region, Link to build: ${buildUrl}", "Commit Hash: ${build_tag}, Application: ${image}, Environment: ${env}, Region: ${region}", null, null, null, null, null, null)
           }
         }
       }
