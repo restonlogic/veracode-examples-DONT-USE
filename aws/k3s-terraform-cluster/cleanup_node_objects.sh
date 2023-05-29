8e5f4451aa99a432f416b40c15efe69e9775c29c
Brandon Samuel <brandon.samuel2010@gmail.com>
2023-05-28 21:30:17 -0400

    working on snow integration

--- aws/k3s-terraform-cluster/cleanup_node_objects.sh
+++ aws/k3s-terraform-cluster/cleanup_node_objects.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+error_exit()
+{
+    echo "Error: $1"
+    exit 1
+}
+
 threshold_time=$(date -d '30 minutes ago' -u +%Y-%m-%dT%H:%M:%SZ)
 node_list=$(kubectl get nodes -o=json | jq -r '.items[] | select(.status.conditions[] | select(.type=="Ready" and .status!="True")) | .metadata.name + " " + .metadata.creationTimestamp')
 
@@ -10,7 +16,7 @@ if [ $node_list ]; then
 
         if [[ "$creation_time" < "$threshold_time" ]]; then
             echo "Deleting node $node_name..."
-            kubectl delete node "$node_name"
+            kubectl delete node "$node_name" || error_exit "Failed to delete node $node_name, please check."
         else
             echo "Skipping node $node_name. Not eligible for deletion."
         fi
