9cee103cda34e3e74d239480a6c0d13ee4d20b53
Brandon Samuel <brandon.samuel2010@gmail.com>
2023-05-29 20:16:51 -0400

    working on snow integration

--- aws/k3s-terraform-cluster/scripts/cleanup_node_objects.sh
+++ aws/k3s-terraform-cluster/scripts/cleanup_node_objects.sh
@@ -9,7 +9,7 @@ error_exit()
 threshold_time=$(date -d '30 minutes ago' -u +%Y-%m-%dT%H:%M:%SZ)
 node_list=$(kubectl get nodes -o=json | jq -r '.items[] | select(.status.conditions[] | select(.type=="Ready" and .status!="True")) | .metadata.name + " " + .metadata.creationTimestamp')
 
-if [ $node_list ]; then
+if [ "$node_list" ]; then
     while IFS= read -r node_info; do
         node_name=$(echo "$node_info" | awk '{print $1}')
         creation_time=$(echo "$node_info" | awk '{print $2}')
