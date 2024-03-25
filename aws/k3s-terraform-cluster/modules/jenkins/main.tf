10c53dacde29f33ebba80f0a3ed20ca0809b7b1d
madhujoshi <madhu.joshi@gmail.com>
2024-03-25 14:58:40 -0400

    updated jenkins helm version

--- aws/k3s-terraform-cluster/modules/jenkins/main.tf
+++ aws/k3s-terraform-cluster/modules/jenkins/main.tf
@@ -37,7 +37,7 @@ resource "null_resource" "deploy" {
     command = <<EOT
         helm repo add jenkins https://charts.jenkins.io > /dev/null &&
         helm repo update &&
-        helm upgrade --wait --install jenkins jenkins/jenkins --namespace jenkins --create-namespace --version 4.2.17 -f ${local_file.values.filename} &&
+        helm upgrade --wait --install jenkins jenkins/jenkins --namespace jenkins --create-namespace --version 5.1.4 -f ${local_file.values.filename} &&
         kubectl create clusterrolebinding jenkins-sa --clusterrole=cluster-admin --serviceaccount=jenkins:jenkins-sa --dry-run=client -o yaml | kubectl apply -f -
     EOT
   }
