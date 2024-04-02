e55b829e3a0e574950b9e1cd20e38e6a864f89f6
madhujoshi <madhu.joshi@gmail.com>
2024-04-02 13:58:00 -0400

    ga2

--- aws/k3s-terraform-cluster/secret_services/outputs.tf
+++ aws/k3s-terraform-cluster/secret_services/outputs.tf
@@ -2,9 +2,9 @@ output "github_secrets_id" {
   value = module.github_secrets[*].secret_id
 }
 
-output "jenkins_secrets_id" {
-  value = module.jenkins_secrets[*].secret_id
-}
+# output "jenkins_secrets_id" {
+#   value = module.jenkins_secrets[*].secret_id
+# }
 
 output "veracode_secrets_id" {
   value = module.veracode_secrets[*].secret_id
